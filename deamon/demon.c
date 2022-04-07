7d8c5681c581f8bcd6fffde153ea2243955fc446
Bartosz Sietejko <barto32224@wp.pl>
2022-04-07 16:25:10 +0200

    VSC Stuff

--- deamon/demon.c
+++ deamon/demon.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
+#include <sys/types.h>
 #include <sys/stat.h>
+#include <unistd.h>
+
+#include "../headers/checkdirs.h"
 
 
 int main(int argc, char *argv[])
 {
   if(argc < 3)
   {
-    printf("Użycie programu: \n demon <katalog źródłowy> <katalog docelowy> [argumenty opcjonalne]");
+    printf("Użycie programu: \n demon <katalog źródłowy> <katalog docelowy> [argumenty opcjonalne]\n");
+    return 1;
   }
+
+  if(checkdirs(argv) != 0)
+    printf("essa\n");
+
+  printf("Dobre\n");
 }
