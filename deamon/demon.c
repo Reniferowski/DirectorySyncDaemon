f722245db5cedebfda1d24ee1e979893324829c9
Bartosz Sietejko <barto32224@wp.pl>
2022-04-13 14:47:40 +0200

    set sleep time

--- deamon/demon.c
+++ deamon/demon.c
@@ -8,10 +8,24 @@
 
 int main(int argc, char *argv[])
 {
+  int time = 300;
+
   if(argc < 3)
   {
     printf("Użycie programu: \n demon <katalog źródłowy> <katalog docelowy> [argumenty opcjonalne]\n");
     return 1;
+  }
+
+  if(argc > 3 && argv[4][0] == '-')
+  {
+    switch(argv[4][1])
+    {
+      case 't': //set deamon sleep time
+        time = atoi(argv[5])*60;
+        break;
+      default:
+        break;
+    }
   }
 
   if(checkdirs(argv) != 0)
