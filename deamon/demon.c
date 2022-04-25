df144e1c559b2226b2beeaf75ba22b32e8bfd2c9
Bartosz Sietejko <barto32224@wp.pl>
2022-04-25 16:49:52 +0200

    main program changes

--- deamon/demon.c
+++ deamon/demon.c
@@ -6,22 +6,23 @@
 #include <dirent.h>
 #include <signal.h>
 #include <syslog.h>
+#include <string.h>
 
 #include "../headers/checkdirs.h"
 #include "../headers/fileoperations.h"
 
-int zzz = 0;
+int sigCheck = 0; //flag to not send awakening logs two times(from signal and natural)
+int recursive = 0; // -R option flag
 
-void sigusr_handler(int sig)//SIGUSR1 handler
+void sigusr_handler(int sig) //SIGUSR1 handler
 {
   syslog(LOG_INFO, "Daemon awoken by SIGUSR1");
-  zzz = 1;
+  sigCheck = 1;
 }
 
 int main(int argc, char *argv[])
 {
   int time = 300;
-
   if(argc < 3)
   {
     printf("Użycie programu: \n demon <katalog źródłowy> <katalog docelowy> [opcje]\n");
@@ -32,8 +33,11 @@ int main(int argc, char *argv[])
   {
     switch(argv[3][1])
     {
-      case 't': //set deamon sleep time
+      case 't': //set daemon sleep time
         time = atoi(argv[4])*60;
+        break;
+      case 'R': //set checking subdirectories on
+        recursive = 1;
         break;
       default:
         break;
@@ -41,9 +45,12 @@ int main(int argc, char *argv[])
   }
 
   if(checkdirs(argv) != 0)
+  {
     return 1;
+  }
 
   //daemon(1,0);
+  syslog(LOG_INFO, "Daemon initialized.");
   //int fileCount[2];
   //fileCount[0] = 0;
   //fileCount[1] = 0;
@@ -52,34 +59,49 @@ int main(int argc, char *argv[])
   char *fp[2];//files path holder
 
   signal(SIGUSR1, sigusr_handler);
-
   while(1)
   {
-    syslog(LOG_INFO, "Daemon goes to sleep");
-    sleep(time/10);
-    if(zzz == 0)
-      syslog(LOG_INFO, "Daemon awoken");
+    //syslog(LOG_INFO, "Daemon goes to sleep.");
+    sleep(time/30);
+    printf("abc\n");
+    if(sigCheck == 0)
+      //syslog(LOG_INFO, "Daemon awoken.");
     source = opendir(argv[1]);
     target = opendir(argv[2]);
     //countFiles(source, target, fileCount);//number of src files, and number of dst files
     deleteExcessiveFiles(argv);
-
-
     while((sEntry = readdir(source)) != NULL)
     {
       getFilesPath(argv[1], argv[2], sEntry->d_name, fp);
       if(sEntry->d_type != 4)
       {
+        
         if(checkExistence(target,sEntry->d_name) == 1)
         {
           copy(fp);
-          syslog(LOG_INFO, "Daemon copied files");
+          syslog(LOG_INFO, "Daemon copied files.");
         }
         else if(checkExistence(target,sEntry->d_name) == 0 && cmpModificationDate(fp) == 1)
         {
           copy(fp);
-          syslog(LOG_INFO, "Daemon copied files");
+          syslog(LOG_INFO, "Daemon detected modified files, updating.");
         }
+      }
+      else if(recursive == 1)
+      {
+        // if(strcmp(sEntry->d_name,".") != 0 && strcmp(sEntry->d_name,"..") != 0)
+        // {
+        //   if(checkExistence(target,sEntry->d_name) == 1)
+        //   {
+        //     mkdir(fp[1], 0775);
+        //     recursiveCopy(fp);
+        //   }
+        //   else if(checkExistence(target,sEntry->d_name) == 0 && cmpModificationDate(fp) == 1)
+        //   {
+        //     mkdir(fp[1], 0775);
+        //     recursiveCopy(fp);
+        //   }
+        // }
       }
     }
     rewinddir(source);
@@ -88,7 +110,7 @@ int main(int argc, char *argv[])
     //fileCount[1] = 0;
     //closedir(target);
     //closedir(source);
-    zzz = 0;
+    sigCheck = 0;
   }
 }
 
