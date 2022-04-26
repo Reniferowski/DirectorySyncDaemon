bb18f4ff9c61fb377e4de01a75d2440132a0deed
Bartosz Sietejko <barto32224@wp.pl>
2022-04-26 23:03:20 +0200

    prototype changes

--- headers/fileoperations.h
+++ headers/fileoperations.h
@@ -1,16 +1,16 @@
 #ifndef FILEOPERATIONS_H_
 #define FILEOPERATIONS_H_
 
-int copy(char **paths);
+int copy(char *source, char *destination);
 
 void getFilesPath(char *source, char *destination, char *filename, char **paths);
 
-int cmpModificationDate(char **paths);
-
-void recursiveCopy(char **paths);
-
-void mmapCopy(char **paths);
+int cmpModificationDate(char *source, char *destination);
 
 off_t getFileSize(char *path);
+
+void mmapCopy(char *source, char *destination);
+
+void recursiveCopy(char *source, char* destination, off_t filesize);
 
 #endif
